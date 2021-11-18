014cba227bd5c278d2a535b0c0b266742eef459b
Jarmikorn Klengchauy <jarmikorn.k@thoughtworks.com>
2021-11-18 11:19:52 +0700

    create product model

---
+++ RazorPagesValmart/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace RazorPagesValmart.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+    }
+}
