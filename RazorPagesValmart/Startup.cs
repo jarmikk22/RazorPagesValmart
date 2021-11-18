20acbf40f3d334ef32697e4a509e3c4d9e72b7e2
Jarmikorn Klengchauy <jarmikorn.k@thoughtworks.com>
2021-11-18 15:28:28 +0700

    change db to use sql server instead sqlite

--- RazorPagesValmart/Startup.cs
+++ RazorPagesValmart/Startup.cs
@@ -28,7 +28,7 @@ namespace RazorPagesValmart
             services.AddRazorPages().AddRazorRuntimeCompilation();
 
             services.AddDbContext<RazorPagesValmartContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("RazorPagesValmartContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("RazorPagesValmartContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
